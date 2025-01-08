20edf007277b6b7922d1bd8049b54f49c21c66b7
EmpowerSenior <christian.stark1@gmx.de>
2025-01-08 22:06:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,11 +39,3 @@ function displayResults(results) {
         resultsDiv.appendChild(placeElement);
     });
 }
-document.addEventListener('DOMContentLoaded', function() {
-    var searchButton = document.getElementById('searchButton');
-    searchButton.addEventListener('click', function() {
-        var searchInput = document.getElementById('searchInput').value;
-        alert('Sie haben nach: ' + searchInput + ' gesucht.');
-        // Hier können Sie die Logik für die Suche hinzufügen
-    });
-});
